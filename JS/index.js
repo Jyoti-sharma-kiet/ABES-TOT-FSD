d5ea4376a640c334e0ea70cbca918de5b03be579
Jyoti Sharma <jyotijatin231116@gmail.com>
2025-08-20 10:53:33 +0530

    function expression

--- JS/index.js
+++ JS/index.js
@@ -1,9 +1,34 @@
-//var a=12;
-//console.log("hiee="+a);
- let a=10;
-console.log(typeof(a));
-if(a==10){
-    let a=30;
-    console.log("inside if a="+a);
+// // // //var a=12;
+// // // //console.log("hiee="+a);
+// // //  let a=10;
+// // // console.log(typeof a);
+// // // if(a==10){
+// // //     let a=30;
+// // //     console.log("inside if a="+a);
+// // // }
+// // // console.log(a);
+// // let a= 10;
+// // let b = "Jyoti";
+// // if(a===b){
+// //     console.log("inside")
+// // }
+// // else console.log("outside")
+// let val = sum(20,30);
+// function sum(a,b)
+// {
+//     return a+b;
+// }
+
+// console.log("val ="+val)
+// let data = function(){
+//     console.log("hiee")
+// }
+// data();
+const data=()=>{return "function expression"}
+console.log(data())
+const sum = (a,b)=>{
+    return a+b;
 }
-console.log(a);
+console.log(sum(20,30))
+const add = (a,b)=>a+b;
+console.log(add(23,40));
